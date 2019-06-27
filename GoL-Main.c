2b8669528fe5be3b6eb3949fd98f428c3ceda151
lyzerman <ilyabar2000@gmail.com>
2019-06-27 23:37:03 +0300

    Подчистка кода

--- GoL-Main.c
+++ GoL-Main.c
@@ -39,45 +39,9 @@ int nbrs(int board[][BOARD_HEIGHT], int i, int j) {
 	count = 0;
 
 	//обход вокруг клетки
-
 	for (k = -1; k <= 1; k++) for (l = -1; l <= 1; l++)
 		if (k || l)
 			if (board[xadd(i, k)][yadd(j, l)]) count++;
-	return count;
-}
-
-//выведение поля
-void print(int board[][BOARD_HEIGHT]) {
-	int	i, j;
-
-	for (j = 0; j < BOARD_HEIGHT; j++) {
-
-		for (i = 0; i < BOARD_WIDTH; i++) {
-			printf("%c ", board[i][j] ? 'o' : ' ');
-		}
-		printf("\n");
-	}
-}
-
-
-//замена полей
-void cpy(int currgen[BOARD_WIDTH][BOARD_HEIGHT], int nextgen[BOARD_WIDTH][BOARD_HEIGHT]) {
-	for (int i = 0; i < BOARD_WIDTH; i++) for (int j = 0; j < BOARD_HEIGHT; j++) {
-		currgen[i][j] = nextgen[i][j];
-	}
-}
-
-//подсчет кол-ва живых соседей клетки (i,j) на данном массиве
-int nbrs(int board[][BOARD_HEIGHT], int i, int j) {
-	int	k, l, count;
-
-	count = 0;
-
-	//обход вокруг клетки
-
-	for (k = -1; k <= 1; k++) for (l = -1; l <= 1; l++)
-		if (k || l)
-			if (board[i + k][j+l]) count++;
 	return count;
 }
 
@@ -87,7 +51,6 @@ void turn(int currgen[BOARD_WIDTH][BOARD_HEIGHT], int nextgen[BOARD_WIDTH][BOARD
 	int	i, j, a;
 
 	//применяем правила для каждой клетки
-
 	for (i = 0; i < BOARD_WIDTH; i++) for (j = 0; j < BOARD_HEIGHT; j++) {
 		a = nbrs(currgen, i, j);
 		if (a == 2) nextgen[i][j] = currgen[i][j];
@@ -103,6 +66,20 @@ void turn(int currgen[BOARD_WIDTH][BOARD_HEIGHT], int nextgen[BOARD_WIDTH][BOARD
 	}
 }
 
+
+//выведение поля
+void print(int board[][BOARD_HEIGHT]) {
+	int	i, j;
+
+	for (j = 0; j < BOARD_HEIGHT; j++) {
+
+		for (i = 0; i < BOARD_WIDTH; i++) {
+			printf("%c ", board[i][j] ? 'o' : ' ');
+		}
+		printf("\n");
+	}
+}
+
 //проверка отсутствия жизни
 int ifnull(int nextgen[BOARD_WIDTH][BOARD_HEIGHT]){
 	int cnt = 0;
@@ -110,6 +87,13 @@ int ifnull(int nextgen[BOARD_WIDTH][BOARD_HEIGHT]){
 		if (nextgen[i][j] != 0) cnt += 1;
 	}
 	return cnt;
+}
+
+//замена полей
+void cpy(int currgen[BOARD_WIDTH][BOARD_HEIGHT], int nextgen[BOARD_WIDTH][BOARD_HEIGHT]) {
+	for (int i = 0; i < BOARD_WIDTH; i++) for (int j = 0; j < BOARD_HEIGHT; j++) {
+		currgen[i][j] = nextgen[i][j];
+	}
 }
 
 //сравнение текущего и нового поколения на ниличие разницы
@@ -122,32 +106,25 @@ int cmpr(int currgen[BOARD_WIDTH][BOARD_HEIGHT], int nextgen[BOARD_WIDTH][BOARD_
 
 }
 
-//замена полей
-void cpy(int currgen[BOARD_WIDTH][BOARD_HEIGHT], int nextgen[BOARD_WIDTH][BOARD_HEIGHT]) {
-	for (int i = 0; i < BOARD_WIDTH; i++) for (int j = 0; j < BOARD_HEIGHT; j++) {
-		currgen[i][j] = nextgen[i][j];
-	}
-}
-
+//основная программа
 int main() {
-	int	currgen[BOARD_WIDTH][BOARD_HEIGHT], nextgen[BOARD_WIDTH][BOARD_HEIGHT], a;
+	int	currgen[BOARD_WIDTH][BOARD_HEIGHT], nextgen[BOARD_WIDTH][BOARD_HEIGHT], a, b;
 
 	initboard(currgen);
 
 	//процесс игры
-	
 	do {
 		system("cls");
 		print(currgen);
 		turn(currgen, nextgen);
-        a = cmpr(currgen, nextgen);
+		a = cmpr(currgen, nextgen);
 		b = ifnull(nextgen);
-        cpy(currgen, nextgen);
-
-        } while ((a != 0) && (b != 0));//Добавить условия выхода из цикла
-        system("cls");
+		cpy(currgen, nextgen);
+		//getchar();
+		
+	} while ((a != 0) && (b != 0));
+	system("cls");
 	print(nextgen);
 	if (b == 0) printf("Life has stoped, await for the next big bang");
 	if (a == 0) printf("Life has stabilized, nothing to see here anymore");
-	
 }
